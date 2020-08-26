fe9e167a6fbad9125ca85ff293261ace90acef74
Fleu <fleuguinersan@gmail.com>
2020-08-26 19:02:54 +0530

    frontend

--- backend_server/mr_tracker/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MrTrackerConfig(AppConfig):
-    name = 'mr_tracker'
--- frontSide/src/app/BaseUrl/BaseUrl.ts
+++ frontSide/src/app/BaseUrl/BaseUrl.ts
@@ -4,7 +4,8 @@ import { Injectable } from "@angular/core";
     providedIn: 'root'
 })
 export class BaseUrl {
-    url = "http://15.207.175.199/api";
+    // url = "http://15.207.175.199/api";
+    url = 'http://localhost:8000/api'
 
     ALL_API = {
         cart:'cart',
